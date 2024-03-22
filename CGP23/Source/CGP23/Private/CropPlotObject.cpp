e49ac57cc2120d268d7d42cf5d9ca4deb961a2c7
c017176m <114945574+c017176m@users.noreply.github.com>
2024-03-22 20:53:44 +0000

    Added C++ Files
    - Added project files to work with the solutions in C++
    - Added the new crop plot object's .cpp and .h file

---
+++ CGP23/Source/CGP23/Private/CropPlotObject.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CropPlotObject.h"
+
+// Sets default values
+ACropPlotObject::ACropPlotObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACropPlotObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACropPlotObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
